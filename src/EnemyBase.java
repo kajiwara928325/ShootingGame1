259f00de81eb0b2ffac9b68b97562f16558ffd01
kajiwara yuta <shamimomo928325@gmail.com>
2025-06-24 11:10:49 +0900

    エネミーのデザイン

--- src/EnemyBase.java
+++ src/EnemyBase.java
@@ -7,9 +7,18 @@ public class EnemyBase extends Enemy {
 		super.move();
 		if(x>300) vx=-1;
 		if(x<100) vx=1;
+		if (Math.random()<0.05) {
+            GameWorld.enemies.add(new RandomEnemy(x,y,0,1));
+        }
 		if(Math.random()<0.05) {
-			GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
-		}
+            GameWorld.enemies.add(new StraightEnemy(x,y,0,2));
+        }
+		if (Math.random()<0.05) {
+	         GameWorld.enemies.add(new DropEnemy(x,y,0,1));
+	        }
+	        if (Math.random()<0.05) {
+	            GameWorld.enemies.add(new CurveEnemy(x,y,0,1));
+	        }
 	
 	}
 	public void draw(MyFrame f) {
