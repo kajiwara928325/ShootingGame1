04813de3cb2ea373463060cbb33045507686bc95
kajiwara yuta <shamimomo928325@gmail.com>
2025-07-01 10:49:21 +0900

    面によって難易度を変更

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,16 +4,18 @@ public class GameFrame extends MyFrame {
 	public void run() {
 		GameWorld.player=new Player(100,300,0,0);
 		addKeyListener(GameWorld.player);//イベントリスナー登録	
+		GameWorld.stage=1;
 		while(true){
 			GameWorld.player.x=100;
 			GameWorld.player.y=300;
 		
 		GameWorld.playerBullets=new Vector<PlayerBullet>();
 		GameWorld.enemies=new Vector<Enemy>();
-		GameWorld.enemies.add(new EnemyBase(100,50,1,0));
+		GameWorld.enemies.add(new EnemyBase(100,50,GameWorld.stage,0));
 		GameWorld.enterPressed=false;
 		while(true) {
 			clear();
+			drawString("stage = "+GameWorld.stage,300,50,15);
 			GameWorld.player.draw(this);
 			GameWorld.player.move();
 			movePlayerBullets();
@@ -24,12 +26,14 @@ public class GameFrame extends MyFrame {
 				setColor(0,0,0);
 				drawString("クリア!",100,200,40);
 				if(GameWorld.enterPressed) {
+					GameWorld.stage++;
 					break;
 				}
 			}else if(GameWorld.player.y<0) {
 				setColor(0,0,0);
 				drawString("ゲームオーバー!",50,200,40);
 				if(GameWorld.enterPressed) {
+					GameWorld.stage=1;
 					break;
 				}
 			}
